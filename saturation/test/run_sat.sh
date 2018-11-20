fefe7d5cb3c2e488eccf33c336b237bee3b9845d
Rhiannon Jones <rjones0211@hotmail.com>
2018-11-19 23:06:52 -0600

    Fixed run script

--- saturation/test/run_sat.sh
+++ saturation/test/run_sat.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 OUTPUT_DIR=${MRB_SOURCE}/saturation/saturation/output_dir
-WORKING_DIR=${MRB_SOURCE}/saturation/saturation
 
 TEST=/pnfs/sbnd/persistent/users/rsjones/g4_sam_tests/g4-cdffe4ba-0bb0-426d-8901-8e0fc17883e9.root
+TEST_CORSIKA=/pnfs/sbnd/mc/reco/artroot/pre-production/MCP0.9/prodoverlay_corsika_cosmics_cmc_genie_nu_spill_gsimple-configd-v1_tpc/sbndcode/v07_07_00_2_MCP0_9/run_number/00/00/00/01/subrun_number/00/00/00/58/reco-cd71ec1d-d2f6-4ca8-b147-e8a8e33c8b50.root
 TEST_LIST=/pnfs/sbnd/scratch/users/rsjones/g4_file_list.txt
 TEST_RECO_LIST=/pnfs/sbnd/scratch/users/rsjones/reco_file_list.txt
 
@@ -12,7 +12,8 @@ rm -rf $OUTPUT_DIR/*
 cd $OUTPUT_DIR
 
 ###lar -c run_saturation.fcl -s $TEST
+lar -c run_saturation.fcl -s $TEST_CORSIKA
 ###lar -c run_saturation.fcl -S $TEST_LIST
-lar -c run_saturation.fcl -S $TEST_RECO_LIST
+###lar -c run_saturation.fcl -S $TEST_RECO_LIST
 
 cd $WORKING_DIR
